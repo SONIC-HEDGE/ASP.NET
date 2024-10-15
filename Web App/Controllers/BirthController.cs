744ae4050410e5e34cdd92f1ba1a0411e1ecf652
SLASH-PL <hubekhaladyna@gmail.com>
2024-10-15 15:10:08 +0200

    birth

--- Web App/Controllers/BirthController.cs
+++ Web App/Controllers/BirthController.cs
@@ -1,6 +1,34 @@
-﻿namespace Web_App.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_App.Models;
 
-public class BirthController
+namespace Web_App.Controllers;
+
+public class BirthController: Controller
 {
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Result([FromForm] Birth model)
+    {
+        if (!model.isValid())
+        {
+            ViewBag.ErrorMessage = "Brak imienia";
+            return View("CustomError");
+        }
+
+        if (model.Years < 0)
+        {
+            ViewBag.ErrorMessage = "nie możesz mieć przyszłej daty urodzenia";
+            return View("CustomError");
+        }
+        return View(model);
+    }
+    public IActionResult Form()
+    {
+        return View();
+    }
     
 }
--- Web App/Models/Birth.cs
+++ Web App/Models/Birth.cs
@@ -2,5 +2,8 @@
 
 public class Birth
 {
-    
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public bool isValid() => Name != null && Name != String.Empty;
+    public int Years => (int)(DateTime.Now - Date).TotalDays / 365;
 }
