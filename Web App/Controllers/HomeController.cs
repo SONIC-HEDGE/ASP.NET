73b99ef5d9d660f363a6ab35f1f8f6e5e6381703
SLASH-PL <hubekhaladyna@gmail.com>
2024-10-15 12:03:45 +0200

    first commit

--- Web App/Controllers/HomeController.cs
+++ Web App/Controllers/HomeController.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Web_App.Models;
 
-namespace Web_App.Controllers;
+namespace Web_App.Controllers; //dhdhehdue
 
 public class HomeController : Controller
 {
