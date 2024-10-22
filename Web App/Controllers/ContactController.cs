cbdd4e426fcaa4bc51551f03609a20d89b23d8aa
SLASH-PL <hubekhaladyna@gmail.com>
2024-10-22 13:13:48 +0200

    lab3 com2

--- Web App/Controllers/ContactController.cs
+++ Web App/Controllers/ContactController.cs
@@ -1,14 +1,55 @@
 using Microsoft.AspNetCore.Mvc;
+using Web_App.Models;
 
 namespace Web_App.Controllers
 {
     public class ContactController : Controller
     {
-        // GET: Contact
+        //temporary
+        static private Dictionary<int, ContactModel> _contacts = new Dictionary<int, ContactModel>()
+        {
+            {1, new(){Id = 1, Email = "wsei@gmail.com", FirstName = "Adam", LastName = "Brzeziak", BirthDate = new DateOnly(1950, 10, 10), PhoneNumber = "666 666 666"}},
+            {2, new(){Id = 2, Email = "dgshgds@gmail.com", FirstName = "Marcin", LastName = "Pypec", BirthDate = new DateOnly(1930, 10, 10), PhoneNumber = "444 222 666"}}, 
+            {3, new(){Id = 3, Email = "dsdsds@gmail.com", FirstName = "Dupa", LastName = "Debil", BirthDate = new DateOnly(1920, 10, 10), PhoneNumber = "777 111 666"}}
+        };
+
+        private static int currentId = 0;
+        // Lista kontaktów 
         public ActionResult Index()
+        {
+            return View(_contacts);
+        }
+        
+        //Dodanie kontaktu formularz 
+        public ActionResult Add()
         {
             return View();
         }
 
+        //Odebranie danych z formularza i zapisanie w kontaktach
+        [HttpPost]
+        public ActionResult Add(ContactModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);   
+            }
+
+            model.Id = ++currentId;
+            _contacts.Add(model.Id, model);
+
+            return View("Index", _contacts);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            _contacts.Remove(id);
+            return View("Index", _contacts);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(_contacts[id]);
+        }
     }
 }
--- Web App/Models/ContactModel.cs
+++ Web App/Models/ContactModel.cs
@@ -1,6 +1,28 @@
-﻿namespace Web_App.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_App.Models;
 
 public class ContactModel
 {
+    [HiddenInput]
+    public int Id { get; set; }
     
+    [Required(ErrorMessage = "Musisz wpisać bo tak!")]
+    [MaxLength(length: 20, ErrorMessage = "Co tak dużo znaków??!!")]
+    public string FirstName { get; set; }
+    
+    [Required(ErrorMessage = "Musisz wpisać bo tak!")]
+    [MaxLength(length: 50, ErrorMessage = "Co tak dużo znaków??!!")]
+    public string LastName { get; set; }
+    
+    [EmailAddress(ErrorMessage = "Zły email!")]
+    public string Email { get; set; }
+    
+    [Phone(ErrorMessage = "Zły numer telefonu!!")]
+    [RegularExpression("\\d\\d\\d \\d\\d\\d \\d\\d\\d", ErrorMessage = "FORMAT: XXX XXX XXX!!")]
+    public string PhoneNumber { get; set; }
+    
+    [DataType(DataType.Date)]
+    public DateOnly BirthDate { get; set; }
 }
