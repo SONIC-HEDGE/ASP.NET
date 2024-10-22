660906e5166accc468a5ea57c473985c811903e4
SLASH-PL <hubekhaladyna@gmail.com>
2024-10-22 13:13:39 +0200

    lab 3 contacts

---
+++ Web App/Controllers/ContactController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_App.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
